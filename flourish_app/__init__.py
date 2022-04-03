2b932269a31e7d69d946ea5c4cf412492d0eb7f9
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-04-03 18:29:33 +0100

    ITS WORKING NOW

--- flourish_app/__init__.py
+++ flourish_app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask 
 
-from .commands import create_tables
 from .extensions import db
 from .routes.main import main
 
@@ -12,7 +11,5 @@ def create_app(config_file='settings.py'):
     db.init_app(app)
 
     app.register_blueprint(main)
-
-    # app.cli.add_command(create_tables)
 
     return app
