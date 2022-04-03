b60015cac901d242ca7443c8e02c433f837f8e3d
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-04-03 05:35:53 +0100

    update

--- manage.py
+++ manage.py
@@ -1,5 +1,19 @@
+# import os
+# from flask_migrate import Migrate
+
+# from app import app, db
+
+
+# app.config.from_object(os.environ['APP_SETTINGS'])
+
+# migrate = Migrate(app, db)
+
+
+# if __name__ == '__main__':
+#     app.run()
+
 import os
-from flask_migrate import Migrate
+from flask_migrate import Migrate, MigrateCommand
 
 from app import app, db
 
@@ -8,6 +22,8 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 
 migrate = Migrate(app, db)
 
+migrate.add_command('db', MigrateCommand)
+
 
 if __name__ == '__main__':
-    app.run()
+    migrate.run()
